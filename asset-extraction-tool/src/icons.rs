d265a7a01f077220bd06c315d5490df43ce9a696
augenfrosch <45207902+augenfrosch@users.noreply.github.com>
2026-05-12 19:18:00 +0200

    Add initial `extract_icons` implementation

---
+++ asset-extraction-tool/src/icons.rs
@@ -0,0 +1,92 @@
+use std::{error::Error, fmt::Display, fs, path::PathBuf, str::FromStr};
+
+use crate::{ImageFormat, VERSION};
+
+use anyhow::Result;
+use boilmaster_re_exports::asset::{format::Format, service::Service as AssetService};
+use clap::Parser;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Resolution {
+	index: u8,
+}
+
+impl Resolution {
+	pub fn filename_suffix(&self) -> String {
+		match self.index {
+			0 => "".into(),
+			1..=u8::MAX => format!("_{self}"),
+		}
+	}
+}
+
+impl Display for Resolution {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		let index = self.index;
+		match index {
+			0 => write!(f, "lr"),
+			_ => write!(f, "hr{index}"),
+		}
+	}
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct ParseResolutionError;
+
+impl Display for ParseResolutionError {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "Failed to parse resolution")
+	}
+}
+
+impl Error for ParseResolutionError {}
+
+impl FromStr for Resolution {
+	type Err = ParseResolutionError;
+
+	fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
+		match s {
+			"" | "lr" => Ok(Self { index: 0 }),
+			_s if let Some(("", num)) = s.split_once("hr") => Ok(Self {
+				index: num.parse().map_err(|_| ParseResolutionError)?,
+			}),
+			_ => Err(ParseResolutionError),
+		}
+	}
+}
+
+#[derive(Debug, Parser)]
+pub struct IconsArgs {
+	#[arg(short, long, default_value_t = ImageFormat::Png)]
+	format: ImageFormat,
+
+	#[arg(short, long, default_value_t = Resolution { index: 1 }, value_parser = Resolution::from_str)]
+	resolution: Resolution,
+}
+
+pub fn extract_icons(
+	asset_service: AssetService,
+	args: IconsArgs,
+	output_dir: &PathBuf,
+) -> Result<()> {
+	let format = Format::from(args.format);
+	let resolution_suffix = args.resolution.filename_suffix();
+
+	for id in 0..1000000 {
+		let folder_id = id - (id % 1000);
+		let path = format!("ui/icon/{folder_id:0>6}/{id:0>6}{resolution_suffix}.tex");
+
+		let bytes = match asset_service.convert(VERSION, &path, format) {
+			Err(boilmaster_re_exports::asset::error::Error::NotFound(_path)) => continue,
+			err => err?,
+		};
+
+		let output_path = output_dir.join(path);
+		if let Some(parent) = output_path.parent() {
+			fs::create_dir_all(parent)?;
+		}
+		fs::write(output_path.with_extension(format.extension()), bytes)?;
+	}
+
+	Ok(())
+}
--- asset-extraction-tool/src/main.rs
+++ asset-extraction-tool/src/main.rs
@@ -1,14 +1,19 @@
-use std::{fs, path::PathBuf, sync::Arc};
+use std::{fmt::Display, fs, path::PathBuf, sync::Arc};
 
-use anyhow::{Context, Result};
+mod icons;
+use icons::{IconsArgs, extract_icons};
+
+use anyhow::Result;
 use bm_data::Data;
 use bm_version::VersionKey;
 use boilmaster_re_exports::asset::{format::Format, service::Service as AssetService};
-use clap::Parser;
+use clap::{Parser, Subcommand, ValueEnum};
 use ironworks::{
 	Ironworks,
 	sqpack::{Install, SqPack},
 };
+
+const VERSION: VersionKey = VersionKey; // Stand-in version key for the installed version
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -17,18 +22,81 @@ struct Args {
 	/// The directory must be the base directory for the game install, i.e., it must contain the `game` subdirectory.
 	#[arg(short, long = "input", value_name = "PATH")]
 	input_dir: PathBuf,
-
 	/// Set the output directory.
 	///
 	/// Directories are created if they are missing. The default creates a new `output` directory in the current working directory.
 	#[arg(short, long = "output", value_name = "PATH", default_value = "output")]
 	output_dir: PathBuf,
+	#[command(subcommand)]
+	command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+	// /// Export assets
+	// Assets(AssetsArgs),
+	/// Export all icons
+	Icons(IconsArgs),
+	/// Export all maps
+	Maps(MapsArgs),
+}
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
+enum ImageFormat {
+	Jpg,
+	Png,
+	Webp,
+}
+
+impl Display for ImageFormat {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(
+			f,
+			"{}",
+			match self {
+				ImageFormat::Jpg => "jpg",
+				ImageFormat::Png => "png",
+				ImageFormat::Webp => "webp",
+			}
+		)
+	}
+}
+
+impl From<ImageFormat> for Format {
+	fn from(value: ImageFormat) -> Self {
+		match value {
+			ImageFormat::Jpg => Self::Jpeg,
+			ImageFormat::Png => Self::Png,
+			ImageFormat::Webp => Self::Webp,
+		}
+	}
+}
+
+// #[derive(Debug, Parser)]
+// struct AssetsArgs {
+// 	#[arg(short, long, value_name = "ASSET_PATH")]
+// 	path: String,
+// 	#[arg(short, long)]
+// 	convert: bool,
+// 	#[arg(long, required_if_eq("convert", "true"))]
+// 	image_format: Option<ImageFormat>,
+// }
+
+#[derive(Debug, Parser)]
+pub struct MapsArgs {
+	#[arg(short, long, default_value_t = ImageFormat::Png)]
+	format: ImageFormat,
+}
+
+fn extract_maps(asset_service: AssetService, args: MapsArgs, output_dir: &PathBuf) -> Result<()> {
+	Ok(())
 }
 
 fn main() -> Result<()> {
 	let Args {
 		input_dir,
 		output_dir,
+		command,
 	} = Args::parse();
 
 	fs::create_dir_all(&output_dir)?;
@@ -37,19 +105,12 @@ fn main() -> Result<()> {
 	let data = Arc::new(Data {
 		ironworks: Arc::new(ironworks),
 	});
-
 	let asset_service = AssetService::new(data);
-	const VERSION: VersionKey = VersionKey;
 
-	let path = "ui/icon/065000/065002_hr1.tex";
-	let format = Format::Png;
-	let bytes = asset_service.convert(VERSION, path, format)?;
-
-	let output_path = output_dir.join(path);
-	if let Some(parent) = output_path.parent() {
-		fs::create_dir_all(parent)?;
+	match command {
+		Command::Icons(args) => extract_icons(asset_service, args, &output_dir)?,
+		Command::Maps(args) => extract_maps(asset_service, args, &output_dir)?,
 	}
-	fs::write(output_path.with_extension(format.extension()), bytes)?;
 
 	Ok(())
 }
